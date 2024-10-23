bc1e66e0ba405aa7b1db65e7625807960637a8ad
NKSTT <asultamuratov110@gmail.com>
2024-10-23 22:08:45 +0800

    Index misol 10

--- Index10.py
+++ Index10.py
@@ -2,8 +2,13 @@ def main(s):
     """
     A string of five numbers is given. Find the sum of its numbers.
     Args:
-        s(str): parameter
+        s (str): parameter
     Returns:
         int: answer
     """
-    return
+
+    a = int(s[0]) + int(s[1]) + int(s[2]) + int(s[3]) + int(s[4])
+    return a
+
+b = main("22222")
+print(b) 
