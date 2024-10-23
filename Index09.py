0c443ceeddbb5a6bb7a08c3e5b0c587757056d4c
NKSTT <asultamuratov110@gmail.com>
2024-10-23 22:08:30 +0800

    Index misol 9

--- Index09.py
+++ Index09.py
@@ -1,9 +1,6 @@
 def main(s):
-    """
-    a single character string is given. Convert it to int type, return -1 if not possible.
-    Args:
-        s(str): parameter
-    Returns:
-        int: answer
-    """
-    return
+    if s.isdigit():
+        return s
+    else:
+        return -1
+print (main("f"))
